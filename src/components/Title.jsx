db857a3edfef413583d3448a5f9042e49a9029c8
magus <martingramajo08@gmail.com>
2021-09-15 13:17:11 -0300

    first commit

---
+++ src/components/Title.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Title() {
+    return (
+        <div className="text-center text-dark mt-5">
+            <h1>
+                Veterinaria "Mi amigo Leon"
+            </h1>
+            <p className=" text-center text-black-50">
+                administrador de pacientes 
+            </p>
+        </div>
+    )
+}

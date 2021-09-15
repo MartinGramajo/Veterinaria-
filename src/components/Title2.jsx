d201b29ca08bdb3068ca9a05aaaaca78950221ba
magus <martingramajo08@gmail.com>
2021-09-15 15:19:08 -0300

    creo componente title2

---
+++ src/components/Title2.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Title2() {
+    return (
+        <div className="text-center text-dark mt-5">
+            <h1> Administra tus citas </h1>
+            <p className=" text-center text-black-50">
+                "nuestros pacientes son  nuestra prioridad"
+            </p>
+        </div>
+    )
+}
